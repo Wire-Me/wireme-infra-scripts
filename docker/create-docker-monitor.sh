f342a8f4daa52d0fd39a02dcb7308459be21cb30
ip10x <ianpierce42@gmail.com>
2026-04-02 12:44:35 -0500

    uses correct urls

--- docker/create-docker-monitor.sh
+++ docker/create-docker-monitor.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-bash <(curl -fsSL https://raw.githubusercontent.com/your-org/your-repo/main/setup.sh) my-log-stream-name
+echo "Creating Docker Monitor script"
+bash <(curl -fsSL https://raw.githubusercontent.com/Wire-Me/wireme-infra-scripts/refs/heads/main/docker/create-docker-monitor-script.sh)
+
+echo "Creating Docker Monitor service"
+echo "Make sure to provide log stream name as an argument"
+bash <(curl -fsSL https://raw.githubusercontent.com/Wire-Me/wireme-infra-scripts/refs/heads/main/docker/create-docker-monitor-service.sh)
+
